28dda1526787820cccc1a7027f6f18bf6897bcbf
Denis Likhachev <denliemail@yandex.ru>
2025-07-08 21:24:57 +0300

    Initial commit

---
+++ src/main/java/org/example/pp_spring_security_3_1_2/PpSpringSecurity312Application.java
@@ -0,0 +1,13 @@
+package org.example.pp_spring_security_3_1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpSpringSecurity312Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpSpringSecurity312Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/pp_spring_security_3_1_2/PpSpringSecurity312ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pp_spring_security_3_1_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpSpringSecurity312ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
